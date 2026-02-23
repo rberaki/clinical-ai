e118b83dfb87f698be63657d0ea04b165e11072b
rberaki <redae.beraki@gmail.com>
2026-02-22 18:47:51 -0600

    Add alert status constants

---
+++ services/clinical-api-dotnet/src/Clinical.Domain/AlertStatus.cs
@@ -0,0 +1,8 @@
+namespace Clinical.Domain;
+
+public static class AlertStatus
+{
+    public const string Active = "Active";
+    public const string Acknowledged = "Acknowledged";
+    public const string Closed = "Closed";
+}
